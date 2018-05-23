af5d501808841a62c3ba262c726c10aaae7179e7
ra148519 <ra148519@wolverine.lab.ic.unicamp.br>
2018-05-22 22:11:28 -0300

    Criado Get's e Set's para classes Project e Task, alem do construtor para Task.

--- model/src/Project.java
+++ model/src/Project.java
@@ -1,16 +1,74 @@
+import java.util.TreeSet;
 import java.util.SortedSet;
 
+
 public class Project implements Historic, Permited{
+	private static int globalCount = 0;
+	private int id;
     private String name, description;
-    private SortedSet<Task> tasks;
-    private SortedSet<Meeting> toMeet;
-    private SortedSet<Research> research;
+    private TreeSet<Task> tasks;
+    private TreeSet<Meeting> toMeet;
+    private TreeSet<Research> research;
 	
-	public Project (String name, String description, SortedSet<Task> tasks, SortedSet<Meeting> toMeet, SortedSet<Research> research) {
+	public Project (String name, String description) {
 		this.name = name;
 		this.description = description;
+		this.id = globalCount;
+		this.tasks = new TreeSet<Task>();
+		this.toMeet = new TreeSet<Meeting>();
+		this.research = new TreeSet<Research>();
+		globalCount ++;
+		
+	}
+	
+	public void createTask() {
+		Task t = new Task();
+		this.tasks.add(t);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public TreeSet<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(TreeSet<Task> tasks) {
 		this.tasks = tasks;
+	}
+
+	public TreeSet<Meeting> getToMeet() {
+		return toMeet;
+	}
+
+	public void setToMeet(TreeSet<Meeting> toMeet) {
 		this.toMeet = toMeet;
+	}
+
+	public TreeSet<Research> getResearch() {
+		return research;
+	}
+
+	public void setResearch(TreeSet<Research> research) {
 		this.research = research;
 	}
+	
+	
 }
--- model/src/Task.java
+++ model/src/Task.java
@@ -3,6 +3,7 @@ import java.util.SortedMap;
 import java.util.SortedSet;
 
 public class Task implements Permited, Historic{
+	private static int globalCount = 0;
     private int id;
     private String name;
     private String description;
@@ -11,4 +12,79 @@ public class Task implements Permited, Historic{
     private SortedSet<User> watching;
     private SortedSet<Attachment> addons;
     private boolean isDone;
+    
+    public void Task() {
+    
+    }
+    
+    public void Task(String name, String description) {
+    	this.id = globalCount;
+		globalCount ++;
+		this.name = name;
+		this.description = description;
+		this.isDone = false;
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getFinishes() {
+		return finishes;
+	}
+
+	public void setFinishes(Date finishes) {
+		this.finishes = finishes;
+	}
+
+	public SortedSet<Task> getSubTasks() {
+		return subTasks;
+	}
+
+	public void setSubTasks(SortedSet<Task> subTasks) {
+		this.subTasks = subTasks;
+	}
+
+	public SortedSet<User> getWatching() {
+		return watching;
+	}
+
+	public void setWatching(SortedSet<User> watching) {
+		this.watching = watching;
+	}
+
+	public SortedSet<Attachment> getAddons() {
+		return addons;
+	}
+
+	public void setAddons(SortedSet<Attachment> addons) {
+		this.addons = addons;
+	}
+
+	public boolean isDone() {
+		return isDone;
+	}
+
+	public void setDone(boolean isDone) {
+		this.isDone = isDone;
+	}
+    
+    
+    
 }
