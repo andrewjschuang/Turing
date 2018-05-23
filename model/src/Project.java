24ad3186540a7d10abb6d41b9bf8d2b6ea2702e9
ra148519 <ra148519@wolverine.lab.ic.unicamp.br>
2018-05-22 21:42:32 -0300

    Metodo construtor na class Project

--- model/src/Project.java
+++ model/src/Project.java
@@ -1,8 +1,16 @@
 import java.util.SortedSet;
 
 public class Project implements Historic, Permited{
-    private String name, desctiption;
+    private String name, description;
     private SortedSet<Task> tasks;
     private SortedSet<Meeting> toMeet;
     private SortedSet<Research> research;
+	
+	public Project (String name, String description, SortedSet<Task> tasks, SortedSet<Meeting> toMeet, SortedSet<Research> research) {
+		this.name = name;
+		this.description = description;
+		this.tasks = tasks;
+		this.toMeet = toMeet;
+		this.research = research;
+	}
 }
