c4d7e2f0a9d368fd17e0b5ee75ee93f99ce25cf0
ra148519 <ra148519@wolverine.lab.ic.unicamp.br>
2018-05-22 22:20:12 -0300

    Implementado metodos contrutores, get's e set's para classes Version e Research

--- model/src/Research.java
+++ model/src/Research.java
@@ -1,7 +1,51 @@
 import java.util.List;
+import java.util.ArrayList;
 
-public class Research {
-    private String name, decription;
-    private List<String> questions;
-    private List<FeedBack> feedBacks;
+
+public class Research{
+	private String name, decription;
+	private List<String> questions;
+	private List<FeedBack> feedBacks;
+
+	public Research(String name, String decription, List<String> questions, List<FeedBack> feedBacks) {
+		this.name = name;
+		this.decription = decription;
+		this.questions = new ArrayList<String>();
+		this.feedBacks = new ArrayList<FeedBack>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDecription() {
+		return decription;
+	}
+
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+
+	public List<String> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<String> questions) {
+		this.questions = questions;
+	}
+
+	public List<FeedBack> getFeedBacks() {
+		return feedBacks;
+	}
+
+	public void setFeedBacks(List<FeedBack> feedBacks) {
+		this.feedBacks = feedBacks;
+	}
+
+	
+	
 }
--- model/src/Version.java
+++ model/src/Version.java
@@ -4,4 +4,45 @@ import java.util.SortedSet;
 public class Version implements Historic{
     private String name, description;
     private SortedSet<Attachment> attachments;
+    
+    
+	public Version(String name, String description, SortedSet<Attachment> attachments) {
+		this.name = name;
+		this.description = description;
+		this.attachments = attachments;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public SortedSet<Attachment> getAttachments() {
+		return attachments;
+	}
+
+
+	public void setAttachments(SortedSet<Attachment> attachments) {
+		this.attachments = attachments;
+	}
+	
+	
+    
+    
 }
